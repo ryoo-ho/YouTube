e2bdf9c4bd10c5e4e89fd9ccf048a68d2f094710
Dave_Ryoo <fstyle50@naver.com>
2021-11-18 23:15:05 +0900

    fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
-
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div>
-      
-    </div>
-  );
+  const [videos, setVideos] = useState();
+
+  useEffect(() => {
+    var requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+
+    fetch(
+      "https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=motorcycle&key=AIzaSyDJGE2FAB3UDdRRwyNsnhVcmH2SzvA0RJY",
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => setVideos(result.items))
+      .catch((error) => console.log("error", error));
+  }, []);
+
+  return <h1>Hello :)</h1>;
 }
 
 export default App;
